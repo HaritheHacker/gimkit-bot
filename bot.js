5824f4e9e1df7f0f21f298c0afc663d5aa45d069
HaritheHacker <57713286+HaritheHacker@users.noreply.github.com>
2019-11-14 09:00:07 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,19 +1,11 @@
-//How to use this code:
-//Copy and paste this code into the developer console.
-//Type startAnswering() to start the bot (WARNING: Must be on the page that has the question and answer choices)
-//You can also press "s" to start the bot
-//Type stopAnswering() to stop the bot.
-//You can also press "e" to stop the bot.
-//Be warned that the bot will get questions wrong initially. This happens because the bot needs 
-//to guess to figure out the correct answer.
-//The bot will currently not work if the view correct answer setting is off (although it could be improved to work).
+
 
 let moneyPerQuestion = [undefined, 10, 100, 1e3, 1e4, 7.5e4, 3e5, 1e6, 1e7, 1e8]
 let streakBonus = [undefined, 15, 1.5e2, 1.5e3, 1.5e4, 115e3, 450e3, 15e5, 15e6, 2e8]
 let multiplier = [undefined, 50, 300, 2e3, 12e3, 85e3, 7e5, 65e5, 65e6, 1e9]
 
 
-//TODO: Handle view correct answer setting being off.
+
 let results = {}
 const sleep = m => new Promise(r => setTimeout(r, m))
 
@@ -23,7 +15,7 @@ function getMoney() {
 }
 
 async function answerQuestion() {
-	//Element 0 is the question. 1-4 are the answer choices.
+	
 	let elements = document.querySelectorAll("body > div > div > div:nth-child(3) > div:nth-child(1) > div > div > div")
 
 	let questionName = elements[0].innerText
@@ -45,14 +37,14 @@ async function answerQuestion() {
 	
 		let lost = document.querySelector("body > div > div > div:nth-child(3) > div:nth-child(1) > div > div > div").innerText.startsWith("-")
 	
-		//One of shop and viewCorrectAnswer exist
+		
 		if (!lost) {
-			//We got the question correct
+			
 			results[questionName] = guessing
 
 			let money = getMoney()			
 			
-			let shopIndex; //Money per question, streak, multiplier, insurance (not useful).
+			let shopIndex; 
 			
 			let moneyIndex = moneyPerQuestion.findIndex((x) => {return money >= x})
 			let streakIndex = streakBonus.findIndex((x) => {return money >= x})
@@ -84,8 +76,7 @@ async function answerQuestion() {
 			if (shopIndex !== undefined) {
 				let shop = document.querySelector("body > div > div > div:nth-child(3) > div:nth-child(1) > div > div > div:nth-child(2) > span:nth-child(1) > div")
 				shop.click()
-				//TODO: Add powerups.
-				//This little bit of code does not work yet.
+				
 				
 				await sleep(400)
 				
@@ -94,13 +85,13 @@ async function answerQuestion() {
 				
 				await sleep(400)
 				
-				//Indexes 3-12 are purchase options.
+				
 				let selections = document.querySelectorAll("body > div > div > div:nth-child(3) > div:nth-child(1) > div > div > div > div")
-				selections[purchaseIndex + 3].dispatchEvent(new Event("mousedown", {bubbles: true, composed: true})) //Select the upgrade
+				selections[purchaseIndex + 3].dispatchEvent(new Event("mousedown", {bubbles: true, composed: true}))
 				await sleep(300)
-				selections[2].click() //Buy it.
+				selections[2].click() 
 				await sleep(300)
-				document.querySelectorAll("body > div > div > div > div > div")[2].click() //Click to go back to the questions.
+				document.querySelectorAll("body > div > div > div > div > div")[2].click() 
 			}
 			else {
 				let nextQuestion = document.querySelector("body > div > div > div:nth-child(3) > div:nth-child(1) > div > div > div:nth-child(2) > span:nth-child(2) > div")
@@ -114,13 +105,13 @@ async function answerQuestion() {
 			let correctAnswer = document.querySelector("body > div > div > div:nth-child(3) > div:nth-child(1) > div > div > div > div > div:nth-child(3)").innerText
 			results[questionName] = correctAnswer
 			let nextQuestion = document.querySelector("body > div > div > div:nth-child(3) > div:nth-child(1) > div > div > div:nth-child(2)")
-			nextQuestion = nextQuestion.firstElementChild //Not sure why the code was written like this, but OK.
+			nextQuestion = nextQuestion.firstElementChild 
 			nextQuestion.click()
 		}
 	await sleep(400)
 	
 }
-//let oldlog = console.log;window.console.log = function(...data) {oldlog(...data);window.last = data}
+
 
 let answering = false
 
@@ -135,9 +126,6 @@ async function startAnswering() {
 	}
 }
 
-
-
-//Allow pressing s to start
 window.addEventListener("keyup", function(e) {if (e.key === "s") {startAnswering()}})
-//Allow pressing e to end
+
 window.addEventListener("keyup", function(e) {if (e.key === "e") {stopAnswering()}})
